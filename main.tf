845980ef8b2eb7daa4141c6d473a72e90703c62a
Raul Souza <rm355416@fiap.com.br>
2024-08-26 21:41:02 -0300

    FIRST COMMIT

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "aws_iam_role" "lambda_exec_role" {
   })
 
   inline_policy {
-    name   = "lambda-s3-access"
+    name = "lambda-s3-access"
     policy = jsonencode({
       Version = "2012-10-17"
       Statement = [{
